2c3c4080f49de60236bcb5bff15d8e579ccc16b0
iamhamza <hamza.akram07@hotmail.com>
2020-04-16 12:08:19 +0500

    added restful routing structure

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 var express=require("express");
 var app=express();
+
 app.use(express.static(__dirname + '/public'));
+
 app.set("view engine","ejs");
 
+//Index route
 app.get("/",function(req,res){
     res.render("index");
 });
+
+//NEW show form to create new project entry
+//get the data and send it to post campgrounds
+app.get("/post/new",function(req,res){
+    res.render("campgrounds/new");
+});
+
+//Create route
+app.post("/post",function(req,res){
+    res.render("new");
+});
+
+//Show route
+app.get("/post/:id",function(req,res){
+
+});
+
+//edit route
+app.post("post//:id/edit",function(req,res){
+
+});
+
+//put req
+app.put("/post/:id",function(req,res){
+
+});
+
+app.delete("/post/:id",function(req,res){
+
+});
+
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
 
 app.listen(3000,process.env.IP,function(){
     console.log("Server has started");
